a5289599abb7546a5de6718f1f5981f77c3776e7
elanetto <101054274+elanetto@users.noreply.github.com>
2024-01-02 15:21:35 +0100

    javascript homework

---
+++ the-basics/tasks-2jan.js
@@ -0,0 +1,51 @@
+
+// Task 1
+
+var city = "York";
+let age = 35;
+const birthYear = 1988;
+
+console.log(city);
+console.log(age);
+console.log(birthYear);
+
+
+// Task 2
+
+var greeting = "Hello, and welcome!";
+let quantity = 10;
+let isJavaScriptFun = true;
+let thisIsUndefined;
+let emptyValue = null;
+
+console.log(typeof greeting);
+console.log(typeof quantity);
+console.log(typeof isJavaScriptFun);
+console.log(typeof thisIsUndefined);
+console.log(typeof emptyValue);
+
+
+// Task 3
+
+let numString = "25";
+console.log(numString);
+console.log(typeof numString);
+
+let num = parseInt(numString);
+console.log(num);
+console.log(typeof num);
+
+let num2 = 50;
+console.log(num2);
+console.log(typeof num2);
+
+let numString2 = num2.toString();
+console.log(numString2);
+console.log(typeof numString2);
+
+let calculate1 = num + num2;
+console.log(calculate1);
+
+let calculate2 = numString + numString2;
+console.log(calculate2);
+
